d88778a89274c3a6d01e5f91be45c28dbeec964e
jianna-braza <jbraza@uw.edu>
2024-08-25 17:17:28 -0700

    just copy entire section

--- portfolio/src/views/index-sections/AquapodSection.js
+++ portfolio/src/views/index-sections/AquapodSection.js
@@ -1,85 +1,48 @@
-import React from 'react';
+/*!
 
-import {
-  UncontrolledCollapse,
-  NavbarBrand,
-  Navbar,
-  NavItem,
-  NavLink,
-  Nav,
-  Container,
-  Row,
-  Col
-} from "reactstrap";
+=========================================================
+* Paper Kit React - v1.3.2
+=========================================================
 
+* Product Page: https://www.creative-tim.com/product/paper-kit-react
 
-function AquapodSection() {
+* Copyright 2023 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/main/LICENSE.md)
 
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React from "react";
+
+// reactstrap components
+import { Container, Row, Col } from "reactstrap";
+
+// core components
+
+function SectionDark() {
   return (
-    <div>
-      <Navbar className="bg-primary" expand="lg">
-        <Container>
-          <NavbarBrand href="#pablo" onClick={(e) => e.preventDefault()}>
-            The AquaPod (INFO 442)
-          </NavbarBrand>
-          <button
-            className="navbar-toggler navbar-toggler-right burger-menu"
-            data-target="#navbar-primary"
-            id="navbar-primary"
-            type="button"
-          >
-          </button>
-          <UncontrolledCollapse navbar toggler="#navbar-primary">
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink
-                  href="https://github.com/UW-INFO442-AU23/aquanauts"
-                >
-                  <i
-                    aria-hidden={true}
-                    className="nc-icon nc-compass-05"
-                  />
-                  Repository
-                </NavLink>
-              </NavItem>
-            </Nav>
-          </UncontrolledCollapse>
-        </Container>
-      </Navbar>
-      <div className='section' >
+    <>
+      <div className="section section-dark">
         <Container>
           <Row>
-            <Col lg="6" md="12">
-              <img
-                alt="..."
-                className="img-rounded img-responsive"
-                src={require("assets/img/examples/landing-page.png")}
-                style={{ width: "100%" }}
-              />
-            </Col>
-            <Col lg="6" md="12">
-              <h2 className="title">King County Water Resource Locator</h2>
-              <p>
-                This project focuses on Goal 6 from the UN SDGS: Clean Water and Sanitation.
-                This issue arises due to water resources, specifically sanitized and clean drinking
-                water, are not easily accessible to the majority of people.
-                Although access to water is a basic human right, the resources and supplies may not
-                be publicly known, which can be a major barrier for those in need of them. Likewise,
-                there are many capable people who could help donate these supplies or uncover
-                services. However, many people are unaware of how they can assist in this issue.
-                The app aims to serve three purposes for the people of King County, WA:
+            <Col className="ml-auto mr-auto text-center" md="8">
+              <h2 className="title">Completed with examples</h2>
+              <p className="description">
+                The kit comes with three pre-built pages to help you get started
+                faster. You can change the text and images and you're good to
+                go. More importantly, looking at them will give you a picture of
+                what you can built with this powerful kit.
               </p>
-              <ul>
-                <li>Be a resource that people can use to help find clean water resources: water bottle refilling stations, water fountains, public restrooms, etc.</li>
-                <li>Serve as a hub to help promote charities that people can donate to or learn more</li>
-                <li>Educate users of the current state of King County and how water availability affects the community</li>
-              </ul>
             </Col>
           </Row>
         </Container>
       </div>
-    </div>
+    </>
   );
 }
 
-export default AquapodSection;
+export default SectionDark;
